7b1da063524e0da159d8dea10b1355a38a6241c1
Vivinyu <158117941+Vivinyu@users.noreply.github.com>
2024-05-14 19:44:27 -0400

    moved login folder to routes api, added script to login.html, changed comment on routes index.js

--- server.js
+++ server.js
@@ -19,17 +19,17 @@ sequelize.sync({ force: false }).then(() => {
   });
 });
 // Set up sessions with cookies
-//const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {
-//     // Stored in milliseconds
-//     maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize,
-//   }),
-// };
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    // Stored in milliseconds
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-// app.use(session(sess));
+app.use(session(sess));
